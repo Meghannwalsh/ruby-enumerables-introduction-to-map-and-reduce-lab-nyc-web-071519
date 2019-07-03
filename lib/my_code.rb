2cd475f13323a991656d85a727a1643d4fb55481
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-03 21:04:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,6 +30,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
+  counter
+  source_array.each do |i|
+    counter += i
+  end
+  counter
 end
 
 def reduce_to_all_true(source_array)
